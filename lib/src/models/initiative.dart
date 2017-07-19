947f5a856642f39d9a2abcaba3b446e9b3bfae6a
Piyush <piyushgk1@gmail.com>
2017-07-19 14:22:03 -0400

    summary shows a different thing for each year

--- lib/src/models/initiative.dart
+++ lib/src/models/initiative.dart
@@ -23,6 +23,6 @@ class Initiative {
     for (var dir in directives) {
       total += dir.percentage;
     }
-    return total ~/ directives.length ?? 0;
+    return total ~/ directives.length ?? 1;
   }
 }
